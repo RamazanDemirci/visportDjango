bc55e7b79a93d5b8c32d628ec108ac96002b1b6b
RamazanDemirci <radem18@gmail.com>
2020-10-13 19:56:50 +0300

    docker-compose django and mongodb container created successfully

--- football_mongo/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FootballMongoConfig(AppConfig):
-    name = 'football_mongo'
---
+++ sport_api/football/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^api/matches$', views.matches),
+    url(r'^api/persons$', views.persons),
+    url(r'^api/upload/logo$', views.logos),
+    url(r'^api/download/logo$', views.logos),
+    url(r'^api/leagues$', views.leagues),
+    url(r'^api/seasons$', views.seasons),
+    url(r'^api/teams$', views.teams),
+    url(r'^api/standing$', views.standing),
+    url(r'^api/standing_all$', views.standing_all),
+    url(r'^api/fixture$', views.fixture),
+    url(r'^api/match_exist$', views.match_exist),
+
+    #url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tutorial_detail),
+    #url(r'^api/tutorials/published$', views.tutorial_list_published)
+]
